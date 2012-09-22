980ee0b680a6a84a397e6ebc62b63d07b9196b59
Adam Pearce <1wheel@gmail.com>
2012-09-22 02:01:06 +0200

    added missing 'p'

--- gapiWrapper.js
+++ gapiWrapper.js
@@ -211,7 +211,7 @@ function findTeamMembers(team) {
 
 function addButton(bTeam, pTeam){
 	//true if player is currently on team - no join Button displayed
-	if (bTeam == team ) {
+	if (bTeam == pteam ) {
 		document.getElementById(bTeam + "Join").innerHTML = "";
 	}
 	//displays join button
