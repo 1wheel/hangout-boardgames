37990927ba13698a6ae46f3883871882f1ad7a06
Adam Pearce <1wheel@gmail.com>
2012-11-14 00:16:07 +0100

    gameSetup needs name not selection index

--- gapiWrapper.js
+++ gapiWrapper.js
@@ -46,7 +46,7 @@ function startNewGameClick(){
 	console.log("canvas set up");
 	lastSelection = (document.getElementById("gameMenu")) ? document.getElementById("gameMenu").selectedIndex : 0;
 	console.log("last selection: " + lastSelection);
-	gameSetup(lastSelection);
+	gameSetup(gameFunctionList[lastSelection]);
 	console.log("game setup completed");
 	gameStartInfo();
 	console.log("gamestart info success");
